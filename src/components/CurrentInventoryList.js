8ab299d5fed25a005a2984c0c7c2a1f0c2bb2b2e
Kristina Peru <krissy.germanov@gmail.com>
2022-03-03 10:26:43 -0700

    completes deliveribles 1-3

--- src/components/CurrentInventoryList.js
+++ src/components/CurrentInventoryList.js
@@ -1,12 +1,18 @@
 import React from 'react'
+import InventoryItemCard from './InventoryItemCard'
 
-function CurrentInventoryList() {
+function CurrentInventoryList({ inventory, onCardClick }) {
+    const itemCards = inventory.map((itemObj) => (<InventoryItemCard 
+        key={itemObj.id} 
+        item={itemObj}
+        onCardClick={onCardClick}
+    />))
 
     return(
         <div id="current-inventory">
             <h2>Current Inventory</h2>
             <div>
-                {/** Put inventory items here */}
+                {itemCards}
             </div>
         </div>
     );
--- src/components/InventoryManger.js
+++ src/components/InventoryManger.js
@@ -1,13 +1,32 @@
-import React from "react";
+import React, { useState, useEffect} from "react";
 import CurrentInventoryList from "./CurrentInventoryList";
 import ReorderInventoryList from "./ReorderInventoryList"
 
 function InventoryManager() {
 
+    const [inventory, setInventory] = useState([])
+    const baseUrl = "http://localhost:8001"
+    const [reorders, setReorders] = useState([])
+
+    useEffect(() => {
+        fetch(baseUrl + '/inventory')
+        .then(res => res.json())
+        .then(setInventory)
+    }, [])
+
+    function addToReorders(item) {
+        if(!reorders.includes(item))
+        setReorders([...reorders, item])
+    }
+    function removeFromReorders(item) {
+        const newReorders = reorders.filter(reorderItem => reorderItem !== item)
+        setReorders([newReorders])
+    }
+
     return(
         <div className="container">
-            <CurrentInventoryList />
-            <ReorderInventoryList />
+            <CurrentInventoryList inventory={inventory} onCardClick={addToReorders}/>
+            <ReorderInventoryList reorders={reorders} onRemoveClick={removeFromReorders}/>
         </div>
     );
 }
